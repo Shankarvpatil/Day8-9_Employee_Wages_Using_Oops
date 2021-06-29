07b99db05099b42663dcd9a5251f43ddc9cc082d
Shankar Vilas Patil <69981881+Shankarvpatil@users.noreply.github.com>
2021-06-29 20:51:22 +0530

    Update Oops Problem.java

--- Oops Problem.java
+++ Oops Problem.java
@@ -1,1 +1,18 @@
-
+class EmployeeWage {
+    public static void main(String[] args) {
+        System.out.println("Welcome "); 
+        Attendance employee = new Attendance();
+        employee.isPresent();
+        
+    }
+}
+class Attendance {
+    int Full_Time = 1;
+    double empCheck = Math.floor(Math.random() * 10) % 2;
+    void isPresent(){
+        if( empCheck == Full_Time ) 
+			System.out.println("Employee is present");	
+		else 
+			System.out.println("Employee is absent");
+        
+    }
