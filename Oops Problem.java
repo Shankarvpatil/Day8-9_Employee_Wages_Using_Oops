8a61bdb16563b358e187f251ac93ba86697896e0
Shankar Vilas Patil <69981881+Shankarvpatil@users.noreply.github.com>
2021-06-29 21:13:51 +0530

    Update Oops Problem.java

--- Oops Problem.java
+++ Oops Problem.java
@@ -1,18 +1,35 @@
-class EmployeeWage {
-    public static void main(String[] args) {
-        System.out.println("Welcome "); 
-        Attendance employee = new Attendance();
-        employee.isPresent();
-        
-    }
+class EmployeeWage
+{
+	public static void main(String[] args)
+	{
+	   System.out.println("Welcome to Employee Wage Computation");
+           Attendance employee = new Attendance();
+           employee.isPresent();
+
+	}
 }
-class Attendance {
-    int Full_Time = 1;
-    double empCheck = Math.floor(Math.random() * 10) % 2;
-    void isPresent(){
-        if( empCheck == Full_Time ) 
-			System.out.println("Employee is present");	
-		else 
-			System.out.println("Employee is absent");
-        
-    }
+class  Attendance extends Wage
+{
+   int IS_FULL_TIME = 1;
+   double empCheck = Math.floor(Math.random() * 10) % 2;
+   void isPresent()
+   {
+       if ( empCheck == IS_FULL_TIME )
+	{
+                System.out.println("Employee is Present !");
+                System.out.println("Wage is "+dailyWage());
+	}
+	else
+                    System.out.println("Employee is Absent !");
+   }
+
+}
+class Wage
+{
+   final int wagePerHr = 20;
+   final int workHr = 8;
+   int dailyWage()
+   {
+     return wagePerHr * workHr;
+   }
+}
